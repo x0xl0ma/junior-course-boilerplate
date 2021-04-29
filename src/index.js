e2ec2110a372b5910a0205181416869282b8a90c
xoxloma <k0tjaka@yandex.ru>
2021-04-29 22:30:34 +0300

    add ini state guard

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import products from './products.json';
 import './index.css';
 
 const ItemsList = () => {
-  const [items] = useState(products);
+  const [items] = useState(products || []);
 
   return (
     <div>
-      <h2>Список товаров</h2>
+      <h2>Список товаров:</h2>
+      {items.length === 0 && <span>товары отсутствуют</span>}
       <ul>
-        {items.map(item => (
-          <li key={item.id}>{item.name}</li>
-        ))}
+        {items.length > 0 &&
+          items.map(item => <li key={item.id}>{item.name}</li>)}
       </ul>
     </div>
   );
